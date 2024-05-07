e73bc6c0b3542dbf90a589f8ef4f3a9135cb2716
Babkina Tatiana <125186477+TanyaBabkina@users.noreply.github.com>
2024-05-08 00:04:36 +0300

    график есть, но в отдельном файле

--- src/main/java/com/example/WebApp/controllers/GoogleChartsController.java
+++ src/main/java/com/example/WebApp/controllers/GoogleChartsController.java
@@ -1,45 +1,49 @@
 package com.example.WebApp.controllers;
 
-import java.util.Map;
-import java.util.TreeMap;
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//@Controller
-//@RequiredArgsConstructor
-//public class GoogleChartsController {
-//    @GetMapping("/chart")
-//    public String getPieChart(Model model) {
-//        Map<String, Integer> graphData = new TreeMap<>();
-//        graphData.put("Epoch 1", 147);
-//        graphData.put("Epoch 2", 1256);
-//        graphData.put("Epoch 3", 3856);
-//        graphData.put("Epoch 4", 19807);
-//        model.addAttribute("chartData", graphData);
-//        return "google-charts.html";
-//    }
-//}
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-// В вашем контроллере
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
-@RequiredArgsConstructor
 public class GoogleChartsController {
+
     @GetMapping("/chart")
-    public String getPieChart(Model model) throws JsonProcessingException {
-        Map<Integer, Integer> graphData = new TreeMap<>();
-        graphData.put(1, 147);
-        graphData.put(2, 1256);
-        graphData.put(3, 3856);
-        graphData.put(4, 19807);
+    public String showLineChart(Model model) {
+//        List<String> model_names = Arrays.asList("Model_1", "Model_2", "Model_3");
+//        List<Integer> yValues = Arrays.asList(10, 20, 15, 25, 30);
+//        System.out.println(xValues);
+//        model.addAttribute("xValues", xValues);
+//        model.addAttribute("yValues", yValues);
+        String model_names = "Model_1,Model_2,Model_3";
+        // Создаем двумерный список
+        List<List<Integer>> list_values = new ArrayList<>();
 
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonData = mapper.writeValueAsString(graphData);
+        // Добавляем первый внутренний список
+        List<Integer> innerList1 = new ArrayList<>();
+        innerList1.add(1);
+        innerList1.add(2);
+        innerList1.add(3);
+        list_values.add(innerList1);
 
-        model.addAttribute("chartData", jsonData);
-        return "google-charts.html";
+        // Добавляем второй внутренний список
+        List<Integer> innerList2 = new ArrayList<>();
+        innerList2.add(4);
+        innerList2.add(5);
+        innerList2.add(6);
+        list_values.add(innerList2);
+
+        // Добавляем третий внутренний список
+        List<Integer> innerList3 = new ArrayList<>();
+        innerList3.add(7);
+        innerList3.add(8);
+        innerList3.add(9);
+        list_values.add(innerList3);
+        model.addAttribute("list_values", list_values);
+        model.addAttribute("model_name", model_names);
+        return "google-charts";
     }
 }
